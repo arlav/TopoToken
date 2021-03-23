dd12c7ca0db062911377dc0af899fb2f654be607
arlav <dounas@gmail.com>
2021-03-23 12:11:45 +0000

    Create mintToken.py

---
+++ python/mintToken.py
@@ -0,0 +1,4 @@
+contract_address = "0x76c2E322138b6cC9D5Fa6D759f4B06BCB5B08b2B"
+wallet_private_key = "XXXXXXXXX"
+wallet_address = "0x7E01CF301C0F55f0A50100D67cA16aCcD75aE1d8"
+infura_url = "https://ropsten.infura.io/v3/bc0151acb1204cacadd049ab3ac000eb"
